d1927ced36875d1d2a8f5e1935b32966f725a296
Schuyler Martin <schuylermartin45@gmail.com>
2022-08-10 17:14:49 -0600

    Added tracking for no-address records

--- cleanup_csv.py
+++ cleanup_csv.py
@@ -23,6 +23,7 @@ def main():
     args = parser.parse_args()
 
     addrTbl: dict[str, list[str]] = {}
+    noAddrLst: list[str] = []
 
     with open(args.file) as csvFile:
         reader = csv.reader(csvFile)
@@ -33,12 +34,20 @@ def main():
             # Skip blanks
             if not name:
                 continue
+            # Track all records without an address (skipping +1s)
+            if not addr:
+                if not "+1" in name:
+                    noAddrLst.append(name)
+                continue
             if addr not in addrTbl:
                 addrTbl[addr] = [name]
             else:
                 addrTbl[addr].append(name)
 
     print(f"Number of addresses: {len(addrTbl.keys())}")
+    print(f"Number of missing addresses: {len(noAddrLst)}")
+    for name in noAddrLst:
+        print(f"  {name}")
 
     outputRows: list[list[str]] = []
     for addr, nameLst in addrTbl.items():
